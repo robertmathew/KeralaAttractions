0c0fb0b93717407f5c05d5cbfd866cd08ce749d5
Robert Mathew <robu1691@gmail.com>
2016-09-14 17:54:10 +0530

    Updated support libraries

--- app/src/main/java/com/coltan/keralaattractions/DetailActivity.java
+++ app/src/main/java/com/coltan/keralaattractions/DetailActivity.java
@@ -6,6 +6,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -25,7 +26,7 @@ public class DetailActivity extends AppCompatActivity {
 
         Glide.with(this)
                 .load(photo.getPhoto())
-                .centerCrop()
+                .priority(Priority.HIGH)
                 .into(imgPhoto);
 
         tvTitle.setText(photo.getTitle());
